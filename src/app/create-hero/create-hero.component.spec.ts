a799b32aa297d7b74f5f3a2601e07583a495a6b4
james-brattin <james.brattin@gmail.com>
2021-05-02 08:12:54 +0200

    upgraded to angular 11

--- src/app/create-hero/create-hero.component.spec.ts
+++ src/app/create-hero/create-hero.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CreateHeroComponent } from './create-hero.component';
 
@@ -6,7 +6,7 @@ describe('CreateHeroComponent', () => {
   let component: CreateHeroComponent;
   let fixture: ComponentFixture<CreateHeroComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CreateHeroComponent ]
     })
--- src/app/hero-item/hero-item.component.spec.ts
+++ src/app/hero-item/hero-item.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { HeroItemComponent } from './hero-item.component';
 
@@ -6,7 +6,7 @@ describe('HeroItemComponent', () => {
   let component: HeroItemComponent;
   let fixture: ComponentFixture<HeroItemComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ HeroItemComponent ]
     })
